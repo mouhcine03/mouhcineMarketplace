c69c5bda27f37691ac64dd1f06733c62065c4c2c
mouhcine03 <mouhcine.elhasnaoui03@gmail.com>
2025-04-24 22:14:15 +0100

    V1.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  # Avant chaque action Devise (signup, update, etc.), autoriser les nouveaux champs
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # Cette méthode autorise `name` et `phone` pendant l'inscription et la mise à jour
+  def configure_permitted_parameters
+    # Lors de l'inscription
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone])
+    
+    # Lors de la mise à jour du compte
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :name, presence: true
+
+  # Validation pour le téléphone
+  validates :phone, presence: true,
+                    format: { with: /\A\d{10}\z/, message: "doit contenir exactement 10 chiffres" }
+
 end
